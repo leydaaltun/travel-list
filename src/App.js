07e488b7abcd0062a24f72edb1a8f61bd19162e4
leydaaltun <altunleyda@gmail.com>
2024-12-25 18:40:51 +0300

    edited( item-footer): edited stats function

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ export default function App() {
   }
 
   function handleToggleItem(id) {
-    console.log(id)
     setItems(
       (items) => items.map((item) => item.id === id ? { ...item, packed: !item.packed } : item
       ));
@@ -21,8 +20,11 @@ export default function App() {
   return <div className="app">
     <Logo />
     <Form onAddItems={handleAddItems} />
-    <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-    <Stats />
+    <PackingList
+      items={items}
+      onDeleteItem={handleDeleteItem}
+      onToggleItem={handleToggleItem} />
+    <Stats items={items} />
   </div>
 }
 
@@ -36,7 +38,6 @@ function Form({ onAddItems }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e)
 
     if (!description) return;
 
@@ -88,8 +89,22 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   )
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    )
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round(numPacked / numItems * 100);
+
   return <footer className="stats">
-    <em>You have X items on your list, and you already packed X (X%)</em>
+    <em> {percentage === 100 ?
+      `You got everything! Ready to go ✈️` :
+      `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+    </em>
+    
   </footer>
 }
